206aa393d2b70930a9b06186d6a7ecde2ba4a065
Renzo Diaz <renzo.diaz@remadi.net>
2023-04-10 05:41:23 -0500

    Fixed units

--- index.js
+++ index.js
@@ -1,60 +1,74 @@
 'use strict';
 
-var bitcore = module.exports;
+var digibyte = module.exports;
 
 // crypto
-bitcore.crypto = {};
-bitcore.crypto.BN = require('./lib/crypto/bn');
-bitcore.crypto.ECDSA = require('./lib/crypto/ecdsa');
-bitcore.crypto.Hash = require('./lib/crypto/hash');
-bitcore.crypto.Random = require('./lib/crypto/random');
-bitcore.crypto.Point = require('./lib/crypto/point');
-bitcore.crypto.Signature = require('./lib/crypto/signature');
+digibyte.crypto = {};
+digibyte.crypto.BN = require('./lib/crypto/bn');
+digibyte.crypto.ECDSA = require('./lib/crypto/ecdsa');
+digibyte.crypto.Hash = require('./lib/crypto/hash');
+digibyte.crypto.Random = require('./lib/crypto/random');
+digibyte.crypto.Point = require('./lib/crypto/point');
+digibyte.crypto.Signature = require('./lib/crypto/signature');
 
 // encoding
-bitcore.encoding = {};
-bitcore.encoding.Base58 = require('./lib/encoding/base58');
-bitcore.encoding.Base58Check = require('./lib/encoding/base58check');
-bitcore.encoding.BufferReader = require('./lib/encoding/bufferreader');
-bitcore.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
-bitcore.encoding.Varint = require('./lib/encoding/varint');
+digibyte.encoding = {};
+digibyte.encoding.Base58 = require('./lib/encoding/base58');
+digibyte.encoding.Base58Check = require('./lib/encoding/base58check');
+digibyte.encoding.BufferReader = require('./lib/encoding/bufferreader');
+digibyte.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
+digibyte.encoding.Varint = require('./lib/encoding/varint');
+digibyte.encoding.Base32 = require('./lib/encoding/base32');
+digibyte.encoding.Precision = require('./lib/encoding/precision');
 
 // utilities
-bitcore.util = {};
-bitcore.util.buffer = require('./lib/util/buffer');
-bitcore.util.js = require('./lib/util/js');
-bitcore.util.preconditions = require('./lib/util/preconditions');
+digibyte.util = {};
+digibyte.util.buffer = require('./lib/util/buffer');
+digibyte.util.js = require('./lib/util/js');
+digibyte.util.preconditions = require('./lib/util/preconditions');
 
 // errors thrown by the library
-bitcore.errors = require('./lib/errors');
-
-// main digibyte library
-bitcore.Address = require('./lib/address');
-bitcore.Block = require('./lib/block');
-bitcore.MerkleBlock = require('./lib/block/merkleblock');
-bitcore.BlockHeader = require('./lib/block/blockheader');
-bitcore.HDPrivateKey = require('./lib/hdprivatekey.js');
-bitcore.HDPublicKey = require('./lib/hdpublickey.js');
-bitcore.Message = require('./lib/message');
-bitcore.Networks = require('./lib/networks');
-bitcore.Opcode = require('./lib/opcode');
-bitcore.PrivateKey = require('./lib/privatekey');
-bitcore.PublicKey = require('./lib/publickey');
-bitcore.Script = require('./lib/script');
-bitcore.Transaction = require('./lib/transaction');
-bitcore.URI = require('./lib/uri');
-bitcore.Unit = require('./lib/unit');
+digibyte.errors = require('./lib/errors');
 
 // mnemonic
-bitcore.Mnemonic = require('./lib/mnemonic');
+digibyte.Mnemonic = require('./lib/mnemonic');
+
+// main digibyte library
+digibyte.Address = require('./lib/address');
+digibyte.Block = require('./lib/block');
+digibyte.MerkleBlock = require('./lib/block/merkleblock');
+digibyte.BlockHeader = require('./lib/block/blockheader');
+digibyte.HDPrivateKey = require('./lib/hdprivatekey.js');
+digibyte.HDPublicKey = require('./lib/hdpublickey.js');
+digibyte.Message = require('./lib/message');
+digibyte.Networks = require('./lib/networks');
+digibyte.Opcode = require('./lib/opcode');
+digibyte.PrivateKey = require('./lib/privatekey');
+digibyte.PublicKey = require('./lib/publickey');
+digibyte.Script = require('./lib/script');
+digibyte.Transaction = require('./lib/transaction');
+digibyte.URI = require('./lib/uri');
+digibyte.Unit = require('./lib/unit');
+
+// digibyte ecosystem
+digibyte.DigiID = require('./lib/digiid');
+digibyte.AssetLookup = require('./lib/digiassets/lookup');
+digibyte.AssetIssuer = require('./lib/digiassets/issuer');
+digibyte.AssetTransferor = require('./lib/digiassets/trasferor');
+digibyte.MetaData = require('./lib/digiassets/metadata');
+digibyte.Rules = require('./lib/digiassets/rules');
+
+// digibyte extra
+digibyte.Price = require('./lib/price');
+digibyte.Blockbook = require('./lib/blockbook');
 
 // dependencies, subject to change
-bitcore.deps = {};
-bitcore.deps.bnjs = require('bn.js');
-bitcore.deps.bs58 = require('bs58');
-bitcore.deps.Buffer = Buffer;
-bitcore.deps.elliptic = require('elliptic');
-bitcore.deps._ = require('lodash');
+digibyte.deps = {};
+digibyte.deps.bnjs = require('bn.js');
+digibyte.deps.bs58 = require('bs58');
+digibyte.deps.Buffer = Buffer;
+digibyte.deps.elliptic = require('elliptic');
+digibyte.deps._ = require('lodash');
 
 // Internal usage, exposed for testing/advanced tweaking
-bitcore.Transaction.sighash = require('./lib/transaction/sighash');
+digibyte.Transaction.sighash = require('./lib/transaction/sighash');
